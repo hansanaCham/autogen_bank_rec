a14e0a121ab8e7bb8b35f61b4024c69f95f5e848
hansanaCham <hansanacham@gmail.com>
2024-04-06 07:59:03 +0530

    ok

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
+import datetime
 import os
+import tempfile
 from autogen import ConversableAgent
+from autogen.coding import LocalCommandLineCodeExecutor
 
-agent_with_number = ConversableAgent(
-    "agent_with_number",
-    system_message="You are playing a game of guess-my-number. "
-    "In the first game, you have the "
-    "number 53 in your mind, and I will try to guess it. "
-    "If I guess too high, say 'too high', if I guess too low, say 'too low'. ",
+
+
+from typing import Annotated, Literal
+
+Operator = Literal["+", "-", "*", "/"]
+
+
+def calculator(a: int, b: int, operator: Annotated[Operator, "operator"]) -> int:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return int(a / b)
+    else:
+        raise ValueError("Invalid operator")
+
+# Let's first define the assistant agent that suggests tool calls.
+assistant = ConversableAgent(
+    name="Assistant",
+    system_message="You are a helpful AI assistant. "
+    "You can help with simple calculations. "
+    "Return 'TERMINATE' when the task is done.",
     llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
-    max_consecutive_auto_reply=1,  # maximum number of consecutive auto-replies before asking for human input
-    is_termination_msg=lambda msg: "53" in msg["content"],  # terminate if the number is guessed by the other agent
-    human_input_mode="TERMINATE",  # ask for human input until the game is terminated
 )
 
-agent_guess_number = ConversableAgent(
-    "agent_guess_number",
-    system_message="I have a number in my mind, and you will try to guess it. "
-    "If I say 'too high', you should guess a lower number. If I say 'too low', "
-    "you should guess a higher number. ",
-    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+# The user proxy agent is used for interacting with the assistant agent
+# and executes tool calls.
+user_proxy = ConversableAgent(
+    name="User",
+    llm_config=False,
+    is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
     human_input_mode="NEVER",
 )
 
-result = agent_with_number.initiate_chat(
-    agent_guess_number,
-    message="I have a number between 1 and 100. Guess it!",
-)
+# Register the tool signature with the assistant agent.
+assistant.register_for_llm(name="calculator", description="A simple calculator")(calculator)
+
+# Register the tool function with the user proxy agent.
+user_proxy.register_for_execution(name="calculator")(calculator)
+
+
+chat_result = user_proxy.initiate_chat(assistant, message="What is (44232 + 13312 / (232 - 32)) * 5?")
+
